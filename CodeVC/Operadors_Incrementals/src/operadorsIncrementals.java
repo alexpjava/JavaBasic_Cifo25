d148b2ca5d031b92d7fc0d068f8d817a1f17f515
AlexP <alexp.java@gmail.com>
2025-09-09 14:15:22 +0200

    Backup2

---
+++ CodeVC/Operadors_Incrementals/src/operadorsIncrementals.java
@@ -0,0 +1,12 @@
+public class operadorsIncrementals {
+    public static void main(String[] args) throws Exception {
+        
+        int x; int y; 
+        x = 5;
+            System.out.println(x);
+        y = ++x;
+            System.out.println(y); 
+        y = x++; 
+            System.out.println(y);
+    }
+}
